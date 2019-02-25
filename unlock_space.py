aa0f02ae19f4aa0ad2a324362915501630dab59f
Robert McWilliam <rmcw@allmail.net>
2019-02-25 00:20:55 +0000

    First pass at thing to unlock the space.

---
+++ unlock_space.py
@@ -0,0 +1,84 @@
+import machine
+from neopixel import NeoPixel
+import time
+
+import urequests
+import ujson
+import utime
+
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+from secrets import networks, unlock_url
+colours = {'red': (255, 0, 0),
+           'green': (0, 255, 0),
+           'blue': (0, 0, 255),
+           'orange': (255, 165, 0),
+           'off': (0,0,0) }
+
+led_pin = machine.Pin(0, machine.Pin.OUT) # D3
+np = NeoPixel(pin, 1)
+
+button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+
+# light LED red
+np[0] = colours['orange']
+np.write()
+
+def blink(p, col=colours['red'], dur=0.25, rep=3):
+    orig = p[0]
+    for i in range(rep):
+        p[0] = col
+        p.write()
+        time.sleep(dur)
+        p[0] = colours['off']
+        p.write()
+        time.sleep(dur)
+    p[0] = orig
+    p.write()
+
+#connect to wifi
+import network, time
+ap = network.WLAN(network.AP_IF)
+ap.active(False)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+nets = wlan.scan()
+for net in nets:
+    ssid = net[0].decode()
+    if ssid in networks:
+        print("Network found ", ssid)
+        wlan.connect(ssid, networks[ssid])
+        while not wlan.isconnected():
+            time.sleep(1)
+        print("Connected to ", ssid)
+        ip = wlan.ifconfig()[0]
+        print("IP:", ip)
+        np[0] = colours['blue']
+        np.write()
+        break
+    else:
+        print(net[0].decode(), " not in known networks")
+else:
+    print("No known networks seen. Switching off networking")
+    wlan.active(False)
+    np[0] = colours['red']
+    np.write()
+
+if wlan.active():
+    while True:
+        if not button.value():
+            resp = urequests.get(unlock_url)
+            ul = ujson.loads(resp.text)
+            if ul['unlocked']:
+                print("Unlocked")
+                np[0] = colours['green']
+                np[0].write()
+                tiem.sleep(30)
+                np[0] = colours['blue']
+                np[0].write()
+            else:
+                print("Error:", ul['message'])
+                blink(np, colours['red'], rep=5)
